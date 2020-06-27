39a9df56a551d817f7ea42c27d2d0880062e7d24
sato.yuki <hirao11921@yahoo.co.jp>
2020-06-27 04:59:28 +0000

    kadai-micropost-2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favoriters, through: :favorites, source: :users
   
   validates :content, presence: true, length: { maximum: 255 }
